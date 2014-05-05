c75d78af1e0cf0d1f5d656efbf914315c49db23c
Miguel Angel <mitxel@gmail.com>
2014-05-05 18:47:51 +0200

    added preview image, fixed videos

--- fixweb.rb
+++ fixweb.rb
@@ -5,6 +5,11 @@ require 'nokogiri'
 my_heraldo = File.open('heraldo_origin.html').read
 my_heraldo = my_heraldo.gsub("src=\"/","src=\"http://www.heraldo.es/")
 my_heraldo = my_heraldo.gsub("href=\"/","href=\"http://www.heraldo.es/")
+# videos
+my_heraldo = my_heraldo.gsub("data=\"/","data=\"http://www.heraldo.es/")
+my_heraldo = my_heraldo.gsub("\"/uploads","\"http://www.heraldo.es/uploads")
+my_heraldo = my_heraldo.gsub("\"/MODULOS","\"http://www.heraldo.es/MODULOS")
+
 
 # Get data from first new
 my_noked_heraldo = Nokogiri::HTML(my_heraldo)
@@ -14,13 +19,19 @@ pre_title = my_noked_heraldo.xpath('//div/div/div/div/div/strong')[0].text
 author = my_noked_heraldo.xpath('//div/div/p/span')[0].text
 subtext = my_noked_heraldo.xpath('//div/div/div/div/div/div/div')[0].text
 
-# Switch my info
+# sometimes there is an image
+image_to_change = my_noked_heraldo.xpath('//div/div/a/img/@src')[0]
+alt_image_to_change = my_noked_heraldo.xpath('//div/div/a/img/@alt')[0]
+
+# Switch with my own data
 my_heraldo = my_heraldo.gsub(title_to_change,"Sesión de web scraping este jueves en #zaragozarb")
 my_heraldo = my_heraldo.gsub(link_to_change,"http://www.meetup.com/Zaragoza-Ruby-Jam-Sessions/")
 my_heraldo = my_heraldo.gsub(pre_title,"Evento de programación en Zaragoza")
 my_heraldo = my_heraldo.gsub(author,"Efe. Zaragoza")
 my_heraldo = my_heraldo.gsub(subtext,"Esta tarde se explicará el uso de la gema Nokogiri, en La Jamonería")
-
+# Image
+my_heraldo = my_heraldo.gsub(image_to_change,"http://photos4.meetupstatic.com/photos/event/4/9/5/e/600_226578782.jpeg")
+my_heraldo = my_heraldo.gsub(alt_image_to_change,"Sesión de web scraping este jueves en #zaragozarb")
 
 # Rewrite with changes in another file
 File.open("heraldo_new.html", "w") { |io|
@@ -30,3 +41,4 @@ File.open("heraldo_new.html", "w") { |io|
 # puts pre_title
 # puts subtext
 # puts title_to_change
+# puts image_to_change
